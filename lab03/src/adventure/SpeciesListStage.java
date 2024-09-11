c30d38a1c5247d2ba3f69c67ebf066bfba15b373
zbrandt <zachary.brandt@gmail.com>
2024-09-10 22:44:20 -0700

    Finished Lab 03

--- lab03/src/adventure/SpeciesListStage.java
+++ lab03/src/adventure/SpeciesListStage.java
@@ -99,6 +99,11 @@ public class SpeciesListStage implements AdventureStage {
                 copy.remove(o);
             }
         }
-        return similarObjects / listOne.size();
+        if (listOne.size() != 0) {
+            return similarObjects / listOne.size();
+        } else {
+            return 0;
+        }
+
     }
 }

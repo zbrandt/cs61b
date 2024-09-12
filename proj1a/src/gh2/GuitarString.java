8bc137286b33e89a59994fff982c5754350993cb
zbrandt <zachary.brandt@gmail.com>
2024-09-12 11:56:18 -0700

    Tentatively finished Project 1A

--- proj1a/src/gh2/GuitarString.java
+++ proj1a/src/gh2/GuitarString.java
@@ -1,7 +1,6 @@
 package gh2;
 import deque.*;
-
-// TODO: maybe more imports
+import edu.princeton.cs.algs4.StdAudio;
 
 //Note: This file will not compile until you complete the Deque61B implementations
 public class GuitarString {
@@ -12,43 +11,48 @@ public class GuitarString {
     private static final double DECAY = .996; // energy decay factor
 
     /* Buffer for storing sound data. */
-    // TODO: uncomment the following line once you're ready to start this portion
-    // private Deque61B<Double> buffer;
+     private Deque61B<Double> buffer;
 
     /* Create a guitar string of the given frequency.  */
     public GuitarString(double frequency) {
-        // TODO: Initialize the buffer with capacity = SR / frequency. You'll need to
-        //       cast the result of this division operation into an int. For
-        //       better accuracy, use the Math.round() function before casting.
-        //       Your should initially fill your buffer with zeros.
+        this.buffer = new LinkedListDeque61B<>();
+        int capacity = (int) Math.round(SR / frequency);
+        for (int i = 0; i < capacity; i++) {
+            this.buffer.addFirst(0.0);
+        }
+
     }
 
 
     /* Pluck the guitar string by replacing the buffer with white noise. */
     public void pluck() {
-        // TODO: Dequeue everything in buffer, and replace with random numbers
-        //       between -0.5 and 0.5. You can get such a number by using:
-        //       double r = Math.random() - 0.5;
-        //
         //       Make sure that your random numbers are different from each
         //       other. This does not mean that you need to check that the numbers
         //       are different from each other. It means you should repeatedly call
         //       Math.random() - 0.5 to generate new random numbers for each array index.
+        int size = this.buffer.size();
+        for (int i = 0; i < size; i++) {
+            this.buffer.removeFirst();
+        }
+        for (int i = 0; i < size; i++) {
+            this.buffer.addFirst(Math.random() - 0.5);
+        }
+
     }
 
     /* Advance the simulation one time step by performing one iteration of
      * the Karplus-Strong algorithm.
      */
     public void tic() {
-        // TODO: Dequeue the front sample and enqueue a new sample that is
-        //       the average of the two multiplied by the DECAY factor.
-        //       **Do not call StdAudio.play().**
+        StdAudio.play(this.buffer.get(0));
+        double front = this.buffer.removeFirst();
+        double newDouble = ((this.buffer.get(0) + front) / 2) * 0.996;
+        this.buffer.addLast(newDouble);
     }
 
     /* Return the double at the front of the buffer. */
     public double sample() {
-        // TODO: Return the correct thing.
-        return 0;
+        return this.buffer.get(0);
     }
 }
-    // TODO: Remove all comments that say TODO when you're done.
+

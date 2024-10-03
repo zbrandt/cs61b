d5a4fce7c70a6cbe46e2ea21d1e8ddae2291ec8e
zbrandt <zachary.brandt@gmail.com>
2024-10-02 18:34:01 -0700

    lab05: Tenatively completed, passing given tests

--- lab05/src/UnionFind.java
+++ lab05/src/UnionFind.java
@@ -1,37 +1,44 @@
 public class UnionFind {
-    // TODO: Instance variables
+    private int[] items;
 
     /* Creates a UnionFind data structure holding N items. Initially, all
        items are in disjoint sets. */
     public UnionFind(int N) {
-        // TODO: YOUR CODE HERE
+        this.items = new int[N];
+        for (int i = 0; i < this.items.length; i++) {
+            this.items[i] = -1;
+        }
     }
 
     /* Returns the size of the set V belongs to. */
     public int sizeOf(int v) {
-        // TODO: YOUR CODE HERE
-        return -1;
+        return parent(find(v)) * -1;
     }
 
     /* Returns the parent of V. If V is the root of a tree, returns the
        negative size of the tree for which V is the root. */
     public int parent(int v) {
-        // TODO: YOUR CODE HERE
-        return -1;
+        return this.items[v];
     }
 
     /* Returns true if nodes/vertices V1 and V2 are connected. */
     public boolean connected(int v1, int v2) {
-        // TODO: YOUR CODE HERE
-        return false;
+        return find(v1) == find(v2);
     }
 
     /* Returns the root of the set V belongs to. Path-compression is employed
        allowing for fast search-time. If invalid items are passed into this
        function, throw an IllegalArgumentException. */
     public int find(int v) {
-        // TODO: YOUR CODE HERE
-        return -1;
+        if (v >= this.items.length) {
+            throw new IllegalArgumentException("An invalid item was passed in.");
+        }
+        if (parent(v) >= 0) {
+            int root = find(parent(v));
+            this.items[v] = root;
+            return root;
+        }
+        return v;
     }
 
     /* Connects two items V1 and V2 together by connecting their respective
@@ -40,7 +47,17 @@ public class UnionFind {
        root to V2's root. Union-ing an item with itself or items that are
        already connected should not change the structure. */
     public void union(int v1, int v2) {
-        // TODO: YOUR CODE HERE
+        if (v1 != v2 && !connected(v1, v2)) {
+            int root1 = find(v1);
+            int root2 = find(v2);
+            if (sizeOf(root2) < sizeOf(root1)) {
+                this.items[root1] += this.items[root2];
+                this.items[root2] = root1;
+            } else {
+                this.items[root2] += this.items[root1];
+                this.items[root1] = root2;
+            }
+        }
     }
 
 }
